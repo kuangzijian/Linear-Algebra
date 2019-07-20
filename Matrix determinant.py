f2d09ea858c4e612835aca559a59b706ca6d1fa0
Jack Kuang <Jack.Kuang@ama.ab.ca>
2019-07-20 14:05:33 -0600

    Matrix Determinant completed

--- Matrix determinant.py
+++ Matrix determinant.py
@@ -36,3 +36,54 @@ M[:,m-1] = M[:,m-2]
 print(M)
 print('larger M rank is ', np.linalg.matrix_rank(A))
 print('larger M determinant is ', np.linalg.det(A))
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Code challenge: Code challenge: large matrices with row exchanges
+
+#Generate a 6x6 matrix;
+
+M = np.round(np.random.rand(6,6) * 10)
+print(M)
+# -compute the determinant
+print('rank is ', np.linalg.matrix_rank(M))
+print('determinant is ', np.linalg.det(M))
+
+# -swap one row, det again
+print('swap first row with second row')
+M1 = M[[1,0,2,3,4,5]]
+print('new M is\n', M1)
+print('new determinant is ', np.linalg.det(M1))
+# -swap two rows
+print('swap first row with second row')
+M2 = M[[1,2,0,3,4,5]]
+print('new M is\n', M2)
+print('new determinant is ', np.linalg.det(M2))
+
+#Code challenge: determinant of shifted matrices
+# (as shift the matrix further and further towards the identity matrix,
+# the abs of determinant is larger and larger)
+
+#Generate a 6x6 matrix;
+# shift amount (l=lambda)
+lamdas = np.linspace(0, 0.1, 30)
+dets = []
+for deti in range (0, len(lamdas)):
+    for i in range(0, 999):
+        M = np.round(np.random.rand(20,20) * 10)
+
+    #impose a linear dependence
+        M[:,0] = M[:,1]
+
+    #shift the matrix (0->0.1 times the identity matrix) (lambda)
+        M = M + lamdas[deti]*np.eye(20,20)
+
+    #comput the abs(determinant) of the shifted matrix
+        det = []
+        det.append(np.abs(np.linalg.det(M)))
+    dets.append(np.mean(det))
+#repeat 1000 times, take the averge abs(det)
+#plot of det as a function of lambda
+plt.plot(lamdas, dets, 's')
+plt.show()
--- Test.py
+++ Test.py
@@ -1,22 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Code challenge: is this vector in the span of this set?
+#Code challenge: determinant of shifted matrices
+# (as shift the matrix further and further towards the identity matrix,
+# the abs of determinant is larger and larger)
 
-v =np.array([[1,2,3,4]])
-print(v)
+#Generate a 6x6 matrix;
+# shift amount (l=lambda)
+lamdas = np.linspace(0, 0.1, 30)
+dets = []
+for deti in range (0, len(lamdas)):
+    for i in range(0, 999):
+        M = np.round(np.random.rand(20,20) * 10)
 
-S =(np.array([[4,3,6,2],[0,4,0,1]]))
-Sv = np.concatenate((S,v), axis=0)
-print(S)
-print(np.linalg.matrix_rank(S))
-print(Sv)
-print(np.linalg.matrix_rank(Sv))
+    #impose a linear dependence
+        M[:,0] = M[:,1]
 
-T = (np.array([[1,2,2,2],[0,0,1,2]]))
-Tv = np.concatenate((T,v), axis=0)
+    #shift the matrix (0->0.1 times the identity matrix) (lambda)
+        M = M + lamdas[deti]*np.eye(20,20)
 
-print(T)
-print(np.linalg.matrix_rank(T))
-print(Tv)
-print(np.linalg.matrix_rank(Tv))
+    #comput the abs(determinant) of the shifted matrix
+        det = []
+        det.append(np.abs(np.linalg.det(M)))
+    dets.append(np.mean(det))
+#repeat 1000 times, take the averge abs(det)
+#plot of det as a function of lambda
+plt.plot(lamdas, dets, 's')
+plt.show()
+
