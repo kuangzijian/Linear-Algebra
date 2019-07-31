94a872502ff570f04d61cb7bf1db653681f403c3
kuangzijian <kuangzijian1@hotmail.com>
2019-07-30 23:32:42 -0600

    Matrix inverse completed

--- Section 9 Matrix inverse.py
+++ Section 9 Matrix inverse.py
@@ -35,3 +35,192 @@ plt.title('AA$^{-1}$')
 
 plt.show()
 
+# Computing the inverse via row reduction
+
+## notice the difference in implementation in this video using sympy, compared to the previous video using numpy ##
+
+# matrix size
+m = 4
+
+# random integers matrix
+A = Matrix( np.round(10*np.random.randn(m,m)),dtype='float' )
+
+# augment A and identity
+Aaug = Matrix( np.concatenate((A,np.eye(m,m)),axis=1) )
+print('Size of Aaug:',Aaug.shape)
+
+# rref
+Asol = Aaug.rref()
+Asol = Asol[0]
+Ainvrref = Asol[:,m:m*2]
+Ainv = A.inv()
+
+# show the augemented matrices
+plt.subplot(211)
+plt.imshow(matrix2numpy(Aaug,dtype='float'))
+plt.title('A|I'), plt.axis('off')
+
+plt.subplot(212)
+plt.imshow(matrix2numpy(Asol,dtype='float'))
+plt.title('I|A$^{-1}$'), plt.axis('off')
+
+plt.show()
+
+
+# show the square matrices
+plt.subplot(131)
+plt.imshow(matrix2numpy(A,dtype='float'))
+plt.title('Matrix A'), plt.axis('off')
+
+plt.subplot(132)
+plt.imshow(matrix2numpy(Ainvrref,dtype='float'))
+plt.title('A$^{-1}$ from rref'), plt.axis('off')
+
+plt.subplot(133)
+plt.imshow(matrix2numpy(Ainv,dtype='float'))
+plt.title('A$^{-1}$ from inv()'), plt.axis('off')
+
+plt.show()
+
+# Left inverse and right inverse
+
+# m>n for left inverse,
+# m<n for right inverse
+m = 6
+n = 3
+
+# create matrices
+A = np.random.randn(m,n)
+AtA = np.matrix.transpose(A)@A
+AAt = A@np.matrix.transpose(A)
+
+# inspect ranks
+print('Rank of A^TA:', np.linalg.matrix_rank(AtA) )
+print('Rank of AA^T:', np.linalg.matrix_rank(AAt) )
+
+# left inverse
+Aleft = np.linalg.inv(AtA)@np.matrix.transpose(A)
+
+# right inverse
+Aright = np.matrix.transpose(A)@np.linalg.inv(AAt)
+
+# now test!
+I_left  = Aleft  @ A
+I_right = A @ Aright
+
+# and then test using the inverse function
+AtA_inv = np.linalg.inv(AtA)
+I_AtA   = AtA_inv @ AtA
+
+AAt_inv = np.linalg.inv(AAt)
+I_AAt   = AAt_inv @ AAt
+
+
+## show images
+plt.subplot(331)
+plt.imshow(A), plt.axis('off')
+plt.title('A')
+
+plt.subplot(332)
+plt.imshow(AtA), plt.axis('off')
+plt.title('A$^T$A')
+
+plt.subplot(333)
+plt.imshow(AAt), plt.axis('off')
+plt.title('AA$^T$')
+
+plt.subplot(335)
+plt.imshow(Aleft), plt.axis('off')
+plt.title('$(A^TA)^{-1}A^T$')
+
+plt.subplot(336)
+plt.imshow(Aright), plt.axis('off')
+plt.title('$A^T(AA^T)^{-1}$')
+
+plt.subplot(338)
+plt.imshow(I_left), plt.axis('off')
+plt.title('[ $(A^TA)^{-1}A^T ]$  A')
+
+plt.subplot(339)
+plt.imshow(I_right), plt.axis('off')
+plt.title('A  [ $A^T(AA^T)^{-1}$ ]')
+
+plt.show()
+
+# The pseudoinverse, part 1
+
+m = 6
+n = 3
+
+# create matrices
+A = np.random.randn(m,n)
+
+pseudoInvA = np.linalg.pinv(A)
+
+plt.subplot(131)
+plt.imshow(A), plt.axis('off')
+plt.title('A')
+
+plt.subplot(132)
+plt.imshow(pseudoInvA), plt.axis('off')
+plt.title('Pseudoinverse of A')
+
+plt.subplot(133)
+plt.imshow(pseudoInvA@A), plt.axis('off')
+plt.title('A$^*$A')
+
+plt.show()
+
+
+# create random matrix
+n = 50
+A = np.random.randn(n,n)
+
+# make rank deficient by repeating a column
+A[:,n-1] = A[:,n-2]
+
+# rank of A!
+print('rank =',np.linalg.matrix_rank(A))
+
+
+# compute the pseudoinverse
+Ai = np.linalg.pinv(A)
+
+
+# and show the matrices
+plt.subplot(221)
+plt.imshow(A), plt.axis('off')
+plt.title('A')
+
+plt.subplot(222)
+plt.imshow(Ai), plt.axis('off')
+plt.title('A$^*$')
+
+plt.subplot(223)
+plt.imshow(Ai@A), plt.axis('off')
+plt.title('A$^*$A')
+
+plt.subplot(224)
+plt.imshow(A@Ai), plt.axis('off')
+plt.title('AA$^*$')
+
+plt.show()
+
+# Code challenge: pseudo inverse of invertible matrices
+
+#psudo inverse is the same as the "real" inverse for an invertible matrix?
+m = 3
+
+# create matrices
+A = np.round(np.random.randn(m,m)*10)
+print(A)
+
+pseudoInvA = np.linalg.pinv(A)
+print(pseudoInvA)
+print(pseudoInvA@A)
+
+InvA = np.linalg.inv(A)
+print(InvA)
+print(InvA@A)
+
+print(pseudoInvA - InvA)
